69f78a96a59f59309006ac7e25ca9910e9c11c6b
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-03-08 23:20:57 +0100

    Dolaczanie db czesc 2

---
+++ randka/Migrations/20200308214308_InitialCreateDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace randka.Migrations
+{
+    public partial class InitialCreateDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "values",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_values", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "values");
+        }
+
+        // Add-Migration InitialCreateDB    <-zainicjowanie db  w package manager
+        // Update-database   <- aby zakualizowac badz utworzyc baze rzeczywista
+    }
+}
