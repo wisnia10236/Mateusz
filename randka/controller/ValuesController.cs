70824bc3581b8becd41facfd26677566344fed5d
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-03-13 00:05:42 +0100

    Zmiana wysylania do bazy na asynchorniczne

--- randka/controller/ValuesController.cs
+++ randka/controller/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using randka.data;
 using randka.models;
 
@@ -22,52 +23,60 @@ namespace randka.controller
             _context = context;
         }
         // GET: api/Values
-        [HttpGet]
+        /*  kod synchorniczny 
+       [HttpGet]
         public IActionResult GetValues()
         {
             var values =_context.values.ToList(); // sciaga wartosci z bazy danych i wrzuca ja do listy
             return Ok(values);
         }
+        */
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.values.ToListAsync(); // sciaga wartosci z bazy danych i wrzuca ja do listy asynchronicznie
+            return Ok(values);
+        }
 
         // GET: api/Values/5
         [HttpGet("{id}")]
-        public IActionResult GetValue(int id)
+        public async Task<IActionResult> GetValue(int id)
         {
-            var value = _context.values.FirstOrDefault(x => x.id == id); // sciaga wartosc z bazy danych i zwraca ja 
+            var value = await _context.values.FirstOrDefaultAsync(x => x.id == id); // sciaga wartosc z bazy danych i zwraca ja async
             return Ok(value);
         }
 
         // POST: api/Values
         // dodawanie nowej wartosci
         [HttpPost]
-        public IActionResult AddValue([FromBody] Value value)
+        public async Task<IActionResult> AddValue([FromBody] Value value)
         {
             _context.values.Add(value);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(value);
         }
 
         // PUT: api/Values/5
         //edycja danych
         [HttpPut("{id}")]
-        public IActionResult EditValue(int id, [FromBody] Value value)
+        public async Task<IActionResult> EditValue(int id, [FromBody] Value value)
         {
-            var data = _context.values.Find(id); // szukamy ja w bazie i zapisujemy do zmiennej
+            var data = await _context.values.FindAsync(id); // szukamy ja w bazie i zapisujemy do zmiennej
             data.name = value.name;     // podmieniamy ja
             _context.values.Update(data);     // aktualizujemy ja
-            _context.SaveChanges();     // zapisujemy zmiany
+            await _context.SaveChangesAsync();     // zapisujemy zmiany
             return Ok(data);
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public IActionResult DeleteValue(int id)
+        public async Task<IActionResult> DeleteValue(int id)
         {
-            var data = _context.values.Find(id);
+            var data = await _context.values.FindAsync(id);
             if (data == null)           // sprawdza czy jest id w bazie ,jesli nie to zwraca ze nie ma go
                 return NoContent();
             _context.values.Remove(data);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
             return Ok(data);
         }
     }
