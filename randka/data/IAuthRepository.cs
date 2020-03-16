484d543746fe02ed480d362180a400d71fba88e3
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-03-16 22:45:30 +0100

    Utworzenie interface do autoryzacji

---
+++ randka/data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using randka.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace randka.data
+{
+    interface IAuthRepository // logika dla repozytorium dla logowania i rejestracji
+    {
+        Task<User> Login(string username, string password); // metoda zwraca uzytkownika dla ktorego przekazuje login i haslo
+        Task<User> Register(User user, string password); // metoda rejestracji ktora przyjmuje klase user i pass aby go zarejestrowac
+        Task<bool> UserExitst(string username); // sprawdza czy istnieje username taki
+
+    }
+}
