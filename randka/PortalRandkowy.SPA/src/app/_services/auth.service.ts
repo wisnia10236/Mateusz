5a5e57239706cb16784f0394de42734b89c58019
wisnia10236 <56563777+wisnia10236@users.noreply.github.com>
2020-03-23 17:23:39 +0100

    Dodanie metody logowania w angularze

---
+++ randka/PortalRandkowy.SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({ //sluzy do wstrzykiwania do servisu naszego dekoratora
+  providedIn: 'root'
+})
+export class AuthService {
+
+  baseUrl = 'https://localhost:44340/api/auth/'; // adres bazowy dla loginu i hasla
+
+  constructor(private http: HttpClient) { } // wstrzykujemy klienta aby miec do niego dostep
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model) // pobiera odpowiedz ze strony dla authcontrollera na podstawie modelu
+      .pipe(map((Response: any) => { 
+        const user = Response;
+        if (user) {
+          localStorage.setItem('token', user.token);
+        }
+      })); // gdy dostajemy odpowiedz z jakas odpowiedzia (response) to zapisujemy ja i jesli jest to zapisujemy token
+  }
+}
+// services nie jest automatycznie dodawany do app.module i trzeba go dopisac
--- randka/PortalRandkowy.SPA/src/app/app.module.ts
+++ randka/PortalRandkowy.SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 
 @NgModule({
@@ -20,7 +21,7 @@ import { NavComponent } from './nav/nav.component';
     HttpClientModule, // dodanie importu dla angulara aby widzial nasze api
     FormsModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
